6fe99440991719c1e0017c69aa4ad6b453e18117
Jonathan Schweppe <jo.schweppe@gmail.com>
2023-07-26 11:20:24 +0200

    Extends functionality for default page template
    Signed-off-by: Jonathan Schweppe <jo.schweppe@gmail.com>

---
+++ dist/js/glightbox-config.js
@@ -0,0 +1,15 @@
+const lightbox = GLightbox({
+    plyr: {
+        config: {
+          ratio: '16:9', // or '4:3'
+          muted: false,
+          hideControls: true,
+          youtube: {
+            noCookie: true,
+            rel: 0,
+            showinfo: 0,
+            iv_load_policy: 3
+          }
+        }
+    }
+});
