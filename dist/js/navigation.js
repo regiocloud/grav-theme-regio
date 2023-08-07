3f902ad35ca2eeb959f3abc78050ac04401366a5
Jonathan Schweppe <jo.schweppe@gmail.com>
2023-08-07 18:30:17 +0200

    Adds bootstrap
    Signed-off-by: Jonathan Schweppe <jo.schweppe@gmail.com>

--- dist/js/navigation.js
+++ dist/js/navigation.js
@@ -11,20 +11,3 @@ for (var i=0; i<links.length; i++) {
         document.getElementById('hamburger').classList.toggle('is-active');
     })
 }
-
-// function toggleSubmenu(element) {
-//     element.classList.toggle('show');
-// }
-
-// const submenuInitItems = document.querySelectorAll('.submenu-init, .submenu2-init');
-
-// submenuInitItems.forEach(item => {
-//     item.addEventListener('click', function (event) {
-//     event.preventDefault();
-//     const submenu = this.querySelector('ul.submenu, ul.submenu2');
-//     if (submenu) {
-//         toggleSubmenu(submenu);
-//     }
-//     });
-// });
-  
