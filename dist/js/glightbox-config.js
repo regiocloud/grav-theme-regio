9005a87669337029eedd37fd2656acb54ed9ba41
Jonathan Schweppe <jo.schweppe@gmail.com>
2023-08-10 18:53:59 +0200

    Rebuilds services/infoboxes template
    Signed-off-by: Jonathan Schweppe <jo.schweppe@gmail.com>

--- dist/js/glightbox-config.js
+++ dist/js/glightbox-config.js
@@ -1,15 +1,1 @@
-const lightbox = GLightbox({
-    plyr: {
-        config: {
-          ratio: '16:9', // or '4:3'
-          muted: false,
-          hideControls: true,
-          youtube: {
-            noCookie: true,
-            rel: 0,
-            showinfo: 0,
-            iv_load_policy: 3
-          }
-        }
-    }
-});
+const lightbox = GLightbox();
