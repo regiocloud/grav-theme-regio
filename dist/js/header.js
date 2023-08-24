d2cfdcff04c54ca6e50dba5ff77ae5cf32a1c489
Jonathan Schweppe <jo.schweppe@gmail.com>
2023-08-24 16:15:54 +0200

    Continues refactoring and expansion of templates
    Signed-off-by: Jonathan Schweppe <jo.schweppe@gmail.com>

--- dist/js/header.js
+++ dist/js/header.js
@@ -2,7 +2,7 @@ window.onscroll = function() {
     var header = document.getElementById("header");
     if (window.scrollY >= 1) {
         if (header) {
-            header.style.boxShadow = "0px 6px 8px -1px #90989d";
+            header.style.boxShadow = "0px 4px 10px 0 rgba(0,0,0,0.16)";
         }
     } else {
         if (header) {
